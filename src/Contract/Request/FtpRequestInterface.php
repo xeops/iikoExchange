17e787bbef430196e52b65961bb1082ddf7345e3
alexei <ageneralov@iiko.ru>
2022-01-24 17:41:56 +0300

    refactoring

---
+++ src/Contract/Request/FtpRequestInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace iikoExchangeBundle\Contract\Request;
+
+interface FtpRequestInterface
+{
+	public function getFilePath(): string;
+
+	public function getFileName(): string;
+
+	public function getFileContent(): string;
+}
---
+++ src/Library/Connection/Ftp/FtpRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace iikoExchangeBundle\Connection\Ftp;
+
+class FtpRequest implements \iikoExchangeBundle\Contract\Request\FtpRequestInterface
+{
+	private string $filePath;
+	private string $fileName;
+	private string $fileContent;
+
+	public function __construct(string $filePath, string $fileName, string $fileContent)
+	{
+		$this->filePath = $filePath;
+		$this->fileName = $fileName;
+		$this->fileContent = $fileContent;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFilePath(): string
+	{
+		return DIRECTORY_SEPARATOR . trim($this->filePath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFileName(): string
+	{
+		return $this->fileName;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFileContent(): string
+	{
+		return $this->fileContent;
+	}
+
+
+}
