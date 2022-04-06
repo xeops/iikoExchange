82fef5e704e5116bb669b3d397c85cfe3756f74f
alexei <ageneralov@iiko.ru>
2022-04-06 15:14:01 +0300

    check conniction on sftp

--- src/Library/Connection/Ftp/SftpConnection.php
+++ src/Library/Connection/Ftp/SftpConnection.php
@@ -64,7 +64,15 @@ class SftpConnection extends FtpConnection
 	{
 		$this->ftp = new SFTP($this->getConfigValue(self::CONFIG_HOST), $this->getConfigValue(self::CONFIG_PORT), 10);
 
-		$this->ftp->login($this->getConfigValue(self::CONFIG_USERNAME), $this->getConfigValue(self::CONFIG_PASSWORD));
+		if(!$this->ftp->login($this->getConfigValue(self::CONFIG_USERNAME), $this->getConfigValue(self::CONFIG_PASSWORD)))
+		{
+			throw new ConnectionException('Exchange connection failure. Check credentials');
+		}
+
+		if(!$this->ftp->isConnected())
+		{
+			throw new ConnectionException('Exchange connection disconnected');
+		}
 
 		return $this->ftp;
 	}
